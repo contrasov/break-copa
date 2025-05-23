fd64f52a54e2706e239d0cf18a98d1c22cb63717
contrasov <agvdsbs@gmail.com>
2025-05-23 19:22:52 -0300

    new design and results

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,20 +4,17 @@ import { Trophy } from 'lucide-vue-next';
 import { ref, computed } from 'vue';
 
 const competitors = ref([
-    { name: 'Álvaro Santos', victories: 0, defeats: 0, drinks: 0 },
-    { name: 'Mateus Rabelo', victories: 0, defeats: 0, drinks: 0 },
-    { name: 'Pedrão Delas', victories: 0, defeats: 0, drinks: 0 },
-    { name: 'Caio Reviews', victories: 0, defeats: 0, drinks: 0 },
-    { name: 'Don. Julia', victories: 0, defeats: 0, drinks: 0 },
-    { name: 'MooLitle', victories: 0, defeats: 0, drinks: 0 },
-    { name: 'Elen Aquino', victories: 0, defeats: 0, drinks: 0 },
-    { name: 'Alanis', victories: 0, defeats: 0, drinks: 0 },
-    { name: 'Arthur Games', victories: 0, defeats: 0, drinks: 0 },
-    { name: 'Rafael Vale J', victories: 0, defeats: 0, drinks: 0 },
-    { name: 'Paulo Amores', victories: 0, defeats: 0, drinks: 0 },
-    { name: 'Carlos .40', victories: 0, defeats: 0, drinks: 0 },
-    { name: 'Alvilania', victories: 0, defeats: 0, drinks: 0 },
-    { name: 'Gabi', victories: 0, defeats: 0, drinks: 0 },
+    { name: 'Álvaro', victories: 0, defeats: 1, drinks: 1 },
+    { name: 'Mateus', victories: 2, defeats: 0, drinks: 6 },
+    { name: 'Caio', victories: 2, defeats: 1, drinks: 3 },
+    { name: 'Julia', victories: 0, defeats: 1, drinks: 1 },
+    { name: 'Murilo', victories: 0, defeats: 1, drinks: 1 },
+    { name: 'Alanis', victories: 0, defeats: 1, drinks: 1 },
+    { name: 'Arthur', victories: 1, defeats: 1, drinks: 2 },
+    { name: 'Paulo', victories: 4, defeats: 0, drinks: 6 },
+    { name: 'Carlos', victories: 1, defeats: 1, drinks: 2 },
+    { name: 'Alvilania', victories: 0, defeats: 1, drinks: 1 },
+    { name: 'Gabi', victories: 0, defeats: 1, drinks: 1 },
 ]);
 
 const sortedCompetitors = computed(() => {
@@ -27,45 +24,51 @@ const sortedCompetitors = computed(() => {
 
 <template>
     <AppLayout>
-        <section class="flex flex-col items-center justify-center gap-2 w-full">
+        <section class="flex flex-col items-start justify-center gap-3 w-full">
 
-            <div class="p-4 flex rounded-md flex-col items-start justify-center w-full">
+            <div class="flex rounded-md flex-col gap-2 items-start justify-center w-full">
                 <span class="flex flex-row items-center justify-center gap-2">
-                    <h1 class="text-white text-2xl md:text-3xl text-left w-full font-bold">Rank</h1>
+                    <h1 class="text-white text-2xl text-left w-full font-bold">Rank</h1>
                     <Trophy class="text-white text-2xl md:text-3xl text-left w-full font-bold" />
                 </span>
             </div>
 
+            <div class="date">
+                <h1>22/05/2025</h1>
+            </div>
+
             <!-- Lista de competidores -->
-            <div class="w-full bg-[#222222]/80 p-4 rounded-md">
-                <div class="list-disc list-inside flex flex-col gap-3">
-                    <div class="competitors-list" v-for="(competitor, index) in sortedCompetitors" :key="competitor.name">
+            <div class="w-full bg-[#0f0f0f] border border-[#303030] p-4 rounded-md">
+                <div class="list-disc list-inside flex flex-col">
+                    <div class="competitors-list" v-for="(competitor, index) in sortedCompetitors" :key="competitor.name" :class="index == 0 ? 'text text-amber-300' : '*:'" >
                         <div class="flex flex-row gap-2 items-center" >
                             <span class="flex flex-row gap-1 items-center">
-                                <p>🏆</p>
-                                <h1 class="font-bold flex flex-row items-center">{{ index + 1 }} </h1>
+                                <h1 class="text-xl pr-3 font-bold flex flex-row items-center">{{ index + 1 }} </h1>
                             </span>
                             
                             <h1 class="font-bold text-[14px]">{{ competitor.name }}</h1>
                         </div>
 
-                        <div class="flex flex-row font-bold text-[#EA7E19] items-center justify-start text-left w-fit gap-2">
+                        <div class="flex flex-row font-bold text-white items-center justify-start text-left w-fill">
                             
-                            <span class="flex flex-row items-center text-sm text-black font-bold rounded-md px-2 py-1 bg-gradient-to-r from-[#b6fd43] to-[#affb29] justify-center gap-2">
-                                <h1>{{ competitor.victories || '00' }}</h1>
+                            <span class="victory">
+                                <h1>{{ competitor.victories || '0' }}</h1>
                             </span>
 
-                            <span class="flex flex-row text-white text-sm font-bold rounded-md px-2 py-1 bg-gradient-to-r from-[#c7155c] to-[#fd4c8f] justify-center gap-2">
-                                <h1>{{ competitor.defeats || '00' }}</h1>
-                            </span>
+                            <div class="defeat">
+                                <h1>{{ competitor.defeats || '0' }}</h1>
+                            </div>
 
-                            <span class="flex flex-row text-white text-sm font-bold rounded-md px-2 py-1 bg-gradient-to-r from-[#20161a] to-[#140c0f] justify-center gap-2">
-                                <h1>{{ competitor.drinks || '00' }}</h1>
+                            <span class="drinks">
+                                <h1>{{ competitor.drinks || '0' }}</h1>
                             </span>
                         </div>
                     </div>
                 </div>
+
             </div>
+            
+            <p class="w-full justify-center flex text-sm text-gray-500">Tarobinha Ltda.</p>
         
         </section>
     </AppLayout>
